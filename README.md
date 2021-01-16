e1820a439cb025e4afeee4bcc10e226babbe2c1c
RENAT ALIMBEKOV <alimbekovr@hotmail.com>
2021-01-17 00:40:40 +0600

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Web app demo for Pytorch Lungs Segmentation
+
+The code for the network: https://github.com/alimbekovKZ/lungs_segmentation
+
+Train pipeline: https://github.com/alimbekovKZ/lungs_segmentation_train
+
+Web app: [https://lungssegmentation.herokuapp.com/](https://lungssegmentation.herokuapp.com/)
+
+
+## Run locally
+
+```bash
+streamlit run app/app.py
+```
+
+![](https://habrastorage.org/webt/sg/zi/dt/sgzidtgitqcohmko3e7_g-f_c1k.png)
---
+++ app/app.py
@@ -0,0 +1,95 @@
+"""Streamlit web app for lungs segmentation"""
+
+import albumentations as A
+import cv2
+import numpy as np
+import streamlit as st
+import torch
+from lungs_segmentation.pre_trained_models import create_model
+
+st.set_option("deprecation.showfileUploaderEncoding", False)
+
+img_size = 512
+aug = A.Compose([A.Resize(img_size, img_size, interpolation=1, p=1)], p=1)
+
+
+@st.cache(allow_output_mutation=True)
+def cached_model():
+    model = create_model("resnet34")
+    device = torch.device("cpu")
+    model = model.to(device)
+    return model
+
+
+model = cached_model()
+
+st.title("Segment lungs")
+
+# uploaded_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+file = st.sidebar.file_uploader(
+    "Upload your image (jpg, jpeg, or png)", ["jpg", "jpeg", "png"]
+)
+
+
+def img_with_masks(img, masks, alpha, return_colors=False):
+    """
+    returns image with masks,
+    img - numpy array of image
+    masks - list of masks. Maximum 6 masks. only 0 and 1 allowed
+    alpha - int transparency [0:1]
+    return_colors returns list of names of colors of each mask
+    """
+    colors = [
+        [255, 0, 0],
+        [0, 255, 0],
+        [0, 0, 255],
+        [255, 255, 0],
+        [0, 255, 255],
+        [102, 51, 0],
+    ]
+    color_names = ["Red", "greed", "BLue", "Yello", "Light", "Brown"]
+    img = img - img.min()
+    img = img / (img.max() - img.min())
+    img *= 255
+    img = img.astype(np.uint8)
+
+    c = 0
+    for mask in masks:
+        mask = np.dstack((mask, mask, mask)) * np.array(colors[c])
+        mask = mask.astype(np.uint8)
+        img = cv2.addWeighted(mask, alpha, img, 1, 0.0)
+        c = c + 1
+    if return_colors is False:
+        return img
+    else:
+        return img, color_names[0:len(masks)]
+
+
+def inference(model, image, thresh=0.2):
+    model.eval()
+    image = (image - image.min()) / (image.max() - image.min())
+    augs = aug(image=image)
+    image = augs["image"].transpose((2, 0, 1))
+    im = augs["image"]
+    image = np.expand_dims(image, axis=0)
+    image = torch.tensor(image)
+
+    mask = torch.nn.Sigmoid()(model(image.float()))
+    mask = mask[0, :, :, :].cpu().detach().numpy()
+    mask = (mask > thresh).astype("uint8")
+    return im, mask
+
+
+if file is not None:
+    image = cv2.imdecode(np.fromstring(file.read(), np.uint8), 1)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    st.image(image, caption="Before", use_column_width=True)
+    st.write("")
+    st.write("Detecting lungs...")
+    image, mask = inference(model, image, 0.2)
+    st.image(
+        img_with_masks(image, [mask[0], mask[1]], alpha=0.1),
+        caption="Image + mask",
+        use_column_width=True,
+    )
